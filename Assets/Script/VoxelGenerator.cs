ced7ccce642f5ef21c54c3eb361e5ef4946d284a
Mateus Sartori <98856859+Mathiuw@users.noreply.github.com>
2025-02-20 18:14:57 -0300

    Added Scatter Trees

--- Assets/Script/VoxelGenerator.cs
+++ Assets/Script/VoxelGenerator.cs
@@ -1,15 +1,22 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class VoxelGenerator : MonoBehaviour
 {
+    [Header("Terrain")]
     [SerializeField] GameObject voxel;
+    [SerializeField] int worldSizeX = 20;
+    [SerializeField] int worldSizeY = 20;
+    [SerializeField] int noiseHeight = 5;
+    [SerializeField] float detailScale = 8;
+    [SerializeField] float gridOffset = 1.0f;
 
-    int worldSizeX = 40;
-    int worldSizeY = 40;
+    [Header("Trees")]
+    [SerializeField] bool scatterTrees = true;
+    [SerializeField] GameObject tree;
+    [SerializeField] int amountPerChunk = 20;
 
-    int noiseHeight = 5;
-
-    float gridOffset = 1.0f;
+    List<Vector3> blockPositions = new List<Vector3>();
 
     private void Start()
     {
@@ -17,11 +24,40 @@ public class VoxelGenerator : MonoBehaviour
         {
             for (int z = 0; z < worldSizeY; z++)
             {
-                Vector3 pos = new Vector3(x * gridOffset, GenerateNoise(x, z, 8f) * noiseHeight, z * gridOffset);
+                // Voxel block position
+                Vector3 pos = new Vector3(x * gridOffset, GenerateNoise(x, z, detailScale) * noiseHeight, z * gridOffset);
 
-                Instantiate(voxel, pos, Quaternion.identity, transform);
+                // Instantiate voxel block
+                GameObject block = Instantiate(voxel, pos, Quaternion.identity, transform);
+
+                // Add block position to positions list
+                blockPositions.Add(block.transform.position);
             }
         }
+
+        SpawnTrees();
+    }
+
+    private void SpawnTrees() 
+    {
+        for (int c = 0; c < amountPerChunk; c++)
+        {
+            GameObject spawnedTree = Instantiate(tree, GenerateTreeSpawnLocation(), Quaternion.identity, transform);
+        }
+    }
+
+    private Vector3 GenerateTreeSpawnLocation()
+    {
+        int randomIndex = Random.Range(0, blockPositions.Count);
+
+        Vector3 newPos = new Vector3(
+            blockPositions[randomIndex].x,
+            blockPositions[randomIndex].y + 0.5f,
+            blockPositions[randomIndex].z
+        );
+        blockPositions.RemoveAt(randomIndex);
+
+        return newPos;
     }
 
     private float GenerateNoise(int x, int z, float detailScale) 
