f403941a56cc0a291873afc4a197ac09265b265f
Mateus Sartori <98856859+Mathiuw@users.noreply.github.com>
2025-05-07 02:12:55 -0300

    0.30
    -Improved sound event
    -Added approaching mode to sound event

--- Assets/Script/SoundEvent.cs
+++ Assets/Script/SoundEvent.cs
@@ -1,9 +1,20 @@
 using UnityEngine;
 
+[RequireComponent (typeof(AudioSource))]
 public class SoundEvent : MonoBehaviour
 {
-    [SerializeField]AudioSource AudioSource;
+    AudioSource audioSource;
     Player player;
+    Vector3 desiredPosition;
+    [SerializeField] bool approaching = false;
+    [SerializeField] float speed = 1.0f;
+    float x;
+    float z;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     private void Start()
     {
@@ -14,11 +25,28 @@ public class SoundEvent : MonoBehaviour
             Debug.LogError("Cant find player");
             enabled = false;
         }
+
+        x = Random.Range(-10, -15);
+        z = Random.Range(-20, 20);
+
+        desiredPosition = player.transform.position + (player.transform.forward * x) + (player.transform.right * z);
+        transform.position = desiredPosition;
+
+        audioSource.Play();
     }
 
     private void Update()
     {
-        transform.position = player.transform.position + (player.transform.forward * -5);
+        if (approaching)
+        {
+            transform.position = Vector3.Lerp(transform.position, player.transform.position, Time.deltaTime * speed);
+        }
+        else 
+        {
+            transform.position = desiredPosition;
+        }
+
+        Debug.DrawLine(player.transform.position, transform.position, Color.yellow);
     }
 
     private void OnDrawGizmos()
