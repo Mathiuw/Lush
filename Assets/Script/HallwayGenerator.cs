0a0a2731176a8cd8a16cdf1a545ab2227b94624d
Mateus Sartori <98856859+Mathiuw@users.noreply.github.com>
2025-05-26 03:21:07 -0300

    0.32
    -Added end game scene
    -Added end game models
    -Added HallwayGenerator class
    -Added Hallway generation

---
+++ Assets/Script/HallwayGenerator.cs
@@ -0,0 +1,111 @@
+using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(Grid))]
+public class HallwayGenerator : MonoBehaviour
+{
+    Hashtable hallwayPositions = new Hashtable();
+
+    [SerializeField] Transform hallwayPrefab;
+    [SerializeField] int hallwayChunkSize = 4;
+    [SerializeField] int hallwaySize = 4;
+    Transform playerTransform;
+    Grid grid;
+
+    private void Awake()
+    {
+        grid = GetComponent<Grid>();
+        grid.cellSize = new Vector3(hallwayChunkSize, hallwayChunkSize, hallwayChunkSize);
+    }
+
+    private void Start()
+    {
+        Player player = FindAnyObjectByType<Player>();
+
+        if (!player)
+        {
+            GameObject worldPivot = new GameObject("World_Pivot");
+            worldPivot.transform.position = Vector3.zero;
+            playerTransform = worldPivot.transform;
+        }
+        else
+        {
+            playerTransform = player.transform;
+        }
+
+        GenerateHallway();
+    }
+
+    private void Update()
+    {
+        GenerateHallway();
+    }
+
+    private void GenerateHallway()
+    {
+        float cTime = Time.realtimeSinceStartup;
+
+        for (int z = -hallwaySize; z < hallwaySize + 1; z++)
+        {
+            Vector3 spawnPosition = new Vector3(0, 2, (z + grid.WorldToCell(playerTransform.position).z) * hallwayChunkSize);
+
+            if (!hallwayPositions.ContainsKey(spawnPosition))
+            {
+                Transform hallwayTransform = Instantiate(hallwayPrefab, spawnPosition, Quaternion.identity, transform);
+
+                HallwayChunk hallwayChunk = new HallwayChunk(cTime, hallwayTransform);
+                Debug.Log(hallwayChunk.cTimeStamp + " " + hallwayChunk.hallwayTransform.position + " added");
+
+                hallwayPositions.Add(spawnPosition, hallwayChunk);
+            }
+            else
+            {
+                ((HallwayChunk)hallwayPositions[spawnPosition]).cTimeStamp = cTime;
+            }
+        }
+
+        Hashtable newChunkPositions = new Hashtable();
+
+        foreach (HallwayChunk hallwayChunk in hallwayPositions.Values)
+        {
+            if (!hallwayChunk.cTimeStamp.Equals(cTime))
+            {
+                Destroy(hallwayChunk.hallwayTransform.gameObject);
+            }
+            else
+            {
+                newChunkPositions.Add(hallwayChunk.hallwayTransform.position, hallwayChunk);
+            }
+        }
+
+        hallwayPositions = newChunkPositions;
+    }
+
+    private class HallwayChunk 
+    {
+        public float cTimeStamp;
+        public Transform hallwayTransform;
+        
+        public HallwayChunk(float cTimeStamp, Transform hallwayTransform) 
+        {
+            this.cTimeStamp = cTimeStamp;
+            this.hallwayTransform = hallwayTransform;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (grid)
+        {
+            for (int z = -hallwaySize; z < hallwaySize + 1; z++)
+            {
+                Vector3 spawnPosition = new Vector3(0, 2, (z + grid.WorldToCell(playerTransform.position).z) * hallwayChunkSize);
+
+                Gizmos.color = Color.red;
+                Gizmos.DrawSphere(spawnPosition, 1f);
+            }
+        }
+    }
+
+
+}
--- Assets/Script/WorldGenerator.cs
+++ Assets/Script/WorldGenerator.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Grid))]
 public class WorldGenerator : MonoBehaviour
 {
     Transform playerTransform;
